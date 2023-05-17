4ee68dadfb0692667e741e61f13f507cd1a7de44
David <davidcharlesv@gmail.com>
2023-05-17 16:53:30 -0700

    updates

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,17 +1,10 @@
 import org.w3c.dom.ls.LSOutput;
+import tools.User;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        var hello = "hi";
-        System.out.println();
-
-        //CONTROL
-        //For Loop
-        for (int i = 0; i < 5; i++) {
-            //System.out.println(i);
-        }
 
         //DATA STRUCTURES
         //Array
@@ -21,16 +14,27 @@ public class Main {
         //Array List (Resizable)
         ArrayList<String> users = new ArrayList<String>();
         users.add("sam");
-        users.add("frodo");
-        users.add("david");
         users.size();
         //users.get(0);
         //users.set(0, "merry");
         //users.clear();
-        for (String user : users) {
-            System.out.println(user);
+
+        //Class
+        User davey = new User(1, "davey");
+        Simple sam = new Simple("sam");
+        System.out.println(sam.getUserName());
+        
+        //CONTROL
+        //For Loop
+        for (int i = 0; i < 5; i++) {
+            //System.out.println(i);
         }
 
+        for (String user : users) {
+            //System.out.println(user);
+        }
+
+        //SORTING
         Collections.sort(users);
         System.out.println("");
         //Collections.reverse(users);
@@ -39,6 +43,7 @@ public class Main {
             System.out.println(user);
         }
 
+        //DATA
         //Set
         Set<Integer> mySet = new HashSet<Integer>();
 
@@ -48,6 +53,7 @@ public class Main {
         for (int i = 0; i < nums.length; i++) {
             myMap.put(nums[i], i);
         }
+
         myMap.entrySet().forEach(entry->{
             //System.out.println(entry.getKey() + " " + entry.getValue());
         });
@@ -76,24 +82,54 @@ public class Main {
         // print the queue
         System.out.println("Queue: " + queue);
 
-        // remove the element at the front of the queue
+        //remove the element at the front of the queue
         String front = queue.remove();
         System.out.println("Removed element: " + front);
-
-        // print the updated queue
-        System.out.println("Queue after removal: " + queue);
-
-        // add another element to the queue
         queue.add("date");
 
-        // peek at the element at the front of the queue
+        //peek at the element at the front of the queue
         String peeked = queue.peek();
         System.out.println("Peeked element: " + peeked);
 
         // print the updated queue
         System.out.println("Queue after peek: " + queue);
 
+    }
 
+    //METHODS
+    //Count Words
+    public static Map<String, Integer> countWords(String s) {
+        //Hash Map with Default Count
+        String[] words = {"one", "two", "two", "three", "three", "three"};
+
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String word : words) {
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        }
+
+        wordMap.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
+        return wordMap;
+    }
+
+}
+
+class Simple {
+    private String userName;
+
+    public Simple(String userName) {
+        this.userName = userName;
+    }
+
+    //Get Username
+    public String getUserName() {
+        System.out.println(userName);
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
 
